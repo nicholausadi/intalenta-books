34fc42de9cf77f1af77462f36c3596dddaf5e1b7
Nicholaus Adisetyo Purnomo <nicho.purnomo@gmail.com>
2024-08-14 00:46:11 +0700

    Initial Commit

---
+++ Intalenta Books/Intalenta_BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Intalenta_BooksApp.swift
+//  Intalenta Books
+//
+//  Created by Nicholaus Adisetyo Purnomo on 13/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Intalenta_BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
